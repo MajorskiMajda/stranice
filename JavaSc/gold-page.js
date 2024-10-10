1cd11db1ec4c88bff7b47f2ee42bb9dec228813b
MajorskiMajda <majorskimajda98@icloud.com>
2024-10-10 14:56:00 +0200

    Generate Gold Page HTML with JS

---
+++ JavaSc/gold-page.js
@@ -0,0 +1,46 @@
+const products = [{
+    image: './images/earrings1.png',
+    name: 'GOLDEN HORIZON',
+    priceCents: 19500,
+    subName: 'GOLD KNOT EARRINGS'
+},
+{
+    image: './images/silver-earrings.png',
+    name: 'SILVER BLOOM',
+    priceCents: 11500,
+    subName: 'SILVER FLOWER EARRINGS'
+},{
+    image: './images/silver-earrings.png',
+    name: 'SILVER GIFT',
+    priceCents: 10500,
+    subName: 'SILVER GIFT EARRINGS'
+}
+];
+
+let productsHTML = '';
+
+products.forEach((product) => {
+    productsHTML += ` 
+      <div class="product-container">
+        <div class=" earring-img-div">
+          <img
+            class="earring-img"
+            id="photo"
+            onmouseover=""
+            src="${product.image}"
+          />
+        </div>
+        <div class="info-container2">
+          <div class="name-container">
+            <h4 class="earring-name serif-font">${product.name}</h4>
+            <h4 class="price1">$${(product.priceCents /100)}</h4>
+            <p class="earring-description sans-font">${product.subName}</p>
+          </div>
+        </div>
+      </div>
+    `;
+ 
+});
+console.log(productsHTML);
+
+document.querySelector('#js-earrings-container').innerHTML = productsHTML;
